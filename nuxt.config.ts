b80a6427e6afc868221eb12ae7e81cb20f7b14a9
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-06-27 16:15:14 +0200

    chore: update compatibility date to 2026-06-27 in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
     '/': { prerender: true }
   },
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: '2026-06-27',
 
   nitro: {
     preset: 'cloudflare_pages_static',
